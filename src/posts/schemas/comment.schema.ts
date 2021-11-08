184120b6f5de208fecbc674316f465efbab3891d
mohammadraasel <mohammadrasel1221@gmail.com>
2021-11-09 05:58:10 +0600

    add comment schema

--- src/posts/schemas/comment.schema.ts
+++ src/posts/schemas/comment.schema.ts
@@ -3,31 +3,20 @@ import { Transform, Type } from 'class-transformer';
 import { Document, ObjectId } from 'mongoose';
 import * as mongoose from 'mongoose';
 import { User } from '../../users/schemas/user.schema';
-import { Comment } from './comment.schema';
 
-export type PostDocument = Post & Document;
+export type CommentDocument = Comment & Document;
 
 @Schema({ timestamps: true })
-export class Post {
+export class Comment {
   @Transform(({ value }) => value.toString())
   _id: ObjectId;
 
   @Prop()
-  title: string;
-
-  @Prop()
-  description: string;
-
-  @Prop()
-  tags: string[];
+  content: string;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: User.name })
   @Type(() => User)
   author: User;
-
-  @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: Comment.name }])
-  @Type(() => Comment)
-  comments: Comment[];
 }
 
-export const PostSchema = SchemaFactory.createForClass(Post);
+export const CommentSchema = SchemaFactory.createForClass(Comment);
