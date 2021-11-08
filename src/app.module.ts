875af59c55a95ac588555023a2756fc8da783128
mohammadraasel <mohammadrasel1221@gmail.com>
2021-11-09 00:15:05 +0600

    setup docker for development

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-
+console.log(process.env.DSN, 'dsn+++++++++++++++++');
 @Module({
   imports: [
     ConfigModule.forRoot({
